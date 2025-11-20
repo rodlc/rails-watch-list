9b7a136ddd4f81718ac7695c54071cc9e829d26c
Rod Lc <63702959+rodlc@users.noreply.github.com>
2025-11-20 17:12:37 +0100

    Styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
+  # Définit la page d'accueil sur la liste des listes
+  root to: "lists#index" 
+
   resources :lists, only: [:index, :show, :new, :create] do
-    # Imbrication : on a besoin de l'ID de la liste pour créer un bookmark
     resources :bookmarks, only: [:new, :create]
   end
-  
-  # Pas d'imbrication : l'ID du bookmark suffit pour le supprimer
   resources :bookmarks, only: [:destroy]
 end
-  
